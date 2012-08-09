753b932e11b93fc3d8337bbc615bbc8884eca9ff
Tiago Dizaro Santos <kattotds@gmail.com>
2012-08-09 18:16:54 -0300

    Update challenge/application/model/form/team.php

--- challenge/application/model/form/team.php
+++ challenge/application/model/form/team.php
@@ -3,14 +3,17 @@ class Application_Model_Form_Team extends Zend_Form
 {
     public function init()
     {
+        /*basic form to seach matches by team*/
     	$this->setMethod('post');
         $this->setAction('search');
         $team = new Application_Model_Mapper_Team();
         $team_list = $team->getTeamList();
         $teams = new Zend_Form_Element_Select('team');
         $teams ->setLabel('Times:');
+        //bluid a combo box
         $teams ->addMultiOptions($team_list);
         $this->addElement($teams);
+        //bluid a button submit
         $this->addElement('submit','pesquisar');
      }
 

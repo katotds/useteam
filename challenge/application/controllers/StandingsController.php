8136065080b5e5b08d153f0b6fb54157e35d7858
Tiago Dizaro Santos <kattotds@gmail.com>
2012-08-09 18:04:41 -0300

    Update challenge/application/controllers/StandingsController.php

--- challenge/application/controllers/StandingsController.php
+++ challenge/application/controllers/StandingsController.php
@@ -1,38 +1,52 @@
 <?php
 class StandingsController extends Zend_Controller_Action
 {
+    /**********************
+	 *
+	 *
+	 * Controller that fill standings of team
+	 *
+	 *
+	 *
+	 */
     public function indexAction()
     {
        $to = $this->_request->getQuery('to');
        $from = $this->_request->getQuery('from');
+       //build validator of date
        $dateValidator = new Zend_Validate_Date('yyyy-MM-dd');
-
+       //if two dates aren´t empty
        if ($to != '' and $from!='') {
        	       if($dateValidator->isValid($to) and $dateValidator->isValid($from)){
                   $match= new Application_Model_Mapper_Match();
+                  //insert into table team information of win,lost,draw and point
                   $match->populateMatchStandingsBetweenDates($to,$from);
                   $team= new Application_Model_Mapper_Team();
+                  //cath all teams order by point
                   $teamInformation=$team->fetchAllPoints();
                   $this->view->assign('team', $teamInformation);
-
+                  //transform to json
                   $body = $this->getRequest()->getRawBody();
                   $data = Zend_Json::decode($body);
                   $this->getResponse()->setHeader('Content-Type', 'text/x-json');
                   $this->getResponse()->setBody($data);
        	       }
        	       else{
-       	       	  echo"<h1>Data inv�lida!</h1>";
+       	       	//if not true data
+       	       	  echo"<h1>Data inválida!</h1>";
        	       }
 
   	    }
         else{
 
                $match= new Application_Model_Mapper_Match();
+               //insert into table team information of win,lost,draw and point
                $match->populateMatchStandings();
                $team= new Application_Model_Mapper_Team();
+               //cath all teams order by point
                $teamInformation=$team->fetchAllPoints();
                $this->view->assign('team', $teamInformation);
-
+               //transform to json
                $body = $this->getRequest()->getRawBody();
                $data = Zend_Json::decode($body);
                $this->getResponse()->setHeader('Content-Type', 'text/x-json');
@@ -41,3 +55,4 @@ class StandingsController extends Zend_Controller_Action
     }
 }
 ?>
+

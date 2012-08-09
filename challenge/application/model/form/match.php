943de6a0e6b1488e654212cab10009eb03dc687b
Tiago Dizaro Santos <kattotds@gmail.com>
2012-08-09 18:16:28 -0300

    Update challenge/application/model/form/match.php

--- challenge/application/model/form/match.php
+++ challenge/application/model/form/match.php
@@ -3,14 +3,17 @@ class Application_Model_Form_Match extends Zend_Form
 {
     public function init()
     {
+        /*basic form to search matches by round */
     	$this->setMethod('post');
         $this->setAction('search');
         $match = new Application_Model_Mapper_Match();
         $match_list = $match->getRoundList();
         $matchs = new Zend_Form_Element_Select('match');
         $matchs ->setLabel('Rodadas:');
+        //bluid a combo box
         $matchs ->addMultiOptions($match_list);
         $this->addElement($matchs);
+        //bluid a button submit
         $this->addElement('submit','pesquisar');
      }
 
